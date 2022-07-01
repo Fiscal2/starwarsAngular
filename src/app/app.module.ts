1f434ba5e56e77bf686ccdb747cad19e27aca19c
Fiscal2 <zacharyross3@gmail.com>
2022-07-01 14:28:46 -0400

    Adding HttpClientModule, but it ain't working :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from ‘@angular/common/http’;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharacterDetailsComponent } from './character-details/character-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

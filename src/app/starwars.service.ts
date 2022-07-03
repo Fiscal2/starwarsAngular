1a97ce12cc42332438bed37a49855fb55bc7e222
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-07-03 11:52:58 -0400

    observe

--- src/app/starwars.service.ts
+++ src/app/starwars.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class StarwarsService {
 
   constructor(private http: HttpClient) { }
 
-  public getSpecificCharacter() {
-    return this.http.get(`${this.BASE_API_URL}/people/1/`);
+  public getSpecificCharacter(): Observable<any> {
+    return this.http.get<any>(`${this.BASE_API_URL}/people/1/`);
   }
 }

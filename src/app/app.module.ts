55c0906b84d6d11a340226c1b3234a62e718888b
Fiscal2 <zacharyross3@gmail.com>
2022-07-03 17:42:07 -0400

    added navbar component :D

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterDetailsComponent
+    CharacterDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

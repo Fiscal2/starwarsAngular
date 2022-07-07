8187984a6ec397570c5325f8480cb4790f082f5c
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-07-07 14:29:16 -0400

    ok

--- src/app/height-converter.pipe.ts
+++ src/app/height-converter.pipe.ts
@@ -6,6 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class HeightConverterPipe implements PipeTransform {
 
   transform(heightInCM: number): string {
+    const totalInches = Math.round(heightInCM / 2.54);
+    const feet = Math.floor(totalInches / 12);
+    const inches = totalInches % 12
+
     return "null";
   }
 

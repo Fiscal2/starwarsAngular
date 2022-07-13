54510dfad9cac1e585913cc636168c3430889eae
Alphabetsoup16 <81780117+Alphabetsoup16@users.noreply.github.com>
2022-07-12 20:48:49 -0400

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { HomeComponent } from './home/home.component';
+import { StarshipDetailsComponent } from './starship-details/starship-details.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'characters', component: CharacterDetailsComponent},
-  // {path: 'starship', component: }
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'characters', component: CharacterDetailsComponent },
+  { path: 'starship', component: StarshipDetailsComponent }
 
 ];
 
